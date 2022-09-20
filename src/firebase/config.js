cf805835af72871226b41f2d4d7b21be4d776bc7
Alysson <aharon_curitiba@hotmail.com>
2022-09-19 21:11:39 -0300

    Firebase adicionado

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCIX3MKLabeyHKFYbFpGwgRSAkIlJsTMFc",
+    authDomain: "mini-blog-96388.firebaseapp.com",
+    projectId: "mini-blog-96388",
+    storageBucket: "mini-blog-96388.appspot.com",
+    messagingSenderId: "481346893595",
+    appId: "1:481346893595:web:caec8735319114521345be"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
